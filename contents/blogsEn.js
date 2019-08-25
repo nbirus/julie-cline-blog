5a4e71d3c7c302eb1a9608d9492545ffb3229ef9
Nathan Birus <nathan.birus@nih.gov>
2019-08-25 12:45:48 -0500

    init

---
+++ contents/blogsEn.js
@@ -0,0 +1,3 @@
+export default [
+  'traveling-on-a-budget',
+]
---
+++ locales/i18n-nuxt-config.js
@@ -0,0 +1,21 @@
+const I18N = {
+  useCookie: false,
+  alwaysRedirect: true,
+  locales: [
+    {
+      code: 'en',
+      iso: 'en-US',
+      name: 'English',
+      file: 'en/index.js'
+    },
+  ],
+  lazy: true,
+  seo: false,
+  langDir: '/locales/',
+  defaultLocale: 'en',
+  parsePages: false
+}
+
+module.exports = {
+  I18N
+}
